8eb99a4a851b34e74d83b0e326d06d6585b8188a
Ignasi <i.elgstrom@gmail.com>
2021-07-02 13:35:23 +0200

    Creacio correo i anulacio si poses el cas de bus

--- usuari.js
+++ usuari.js
@@ -1,55 +1,91 @@
 const inquirer = require("inquirer");
 
-inquirer.prompt([
-  {
-    type: "list",
-    message: "¿Que tipo de transporte quieres usar?",
-    name: "tipo",
-    choices: [
-      {
-        name: "Bus",
-        value: "Bus",
-      },
-      {
-        name: "Metro",
-        value: "Metro",
-      },
-    ],
-  },
-  {
-    type: "checkbox",
-    message: "¿Qué información extra quiere obtener de cada parada?",
-    name: "info parada",
-    when: (respuesta) => respuesta.tipo === "Metro",
-    choices: [
-      {
-        name: "Coordenadas",
-        value: "Coordenadas",
-      },
-      {
-        name: "Fecha de inauguración",
-        value: "inauguracion",
-      },
-    ],
-  },
-  {
-    type: "list",
-    message: "¿Quieres que salgan errores?",
-    name: "tipo_yes_no",
-    choices: [
-      {
-        name: "Si",
-        value: "Si",
-      },
-      {
-        name: "No",
-        value: "No",
-      },
-    ],
-  },
-  {
-    type: "input",
-    message: "¿Que línea quieres consultar?",
-    name: "tipo_linea",
-  },
-]);
+const preguntarUsuario = async () => {
+  const respuestas = await inquirer.prompt([
+    {
+      type: "list",
+      message: "¿Que tipo de transporte quieres usar?",
+      name: "tipo",
+      choices: [
+        {
+          name: "Bus",
+          value: "Bus",
+        },
+        {
+          name: "Metro",
+          value: "Metro",
+        },
+      ],
+    },
+    {
+      type: "checkbox",
+      message: "¿Qué información extra quiere obtener de cada parada?",
+      name: "info_parada",
+      when: (respuesta) => respuesta.tipo === "Metro",
+      choices: [
+        {
+          name: "Coordenadas",
+          value: "Coordenadas",
+        },
+        {
+          name: "Fecha de inauguración",
+          value: "inauguracion",
+        },
+      ],
+    },
+    {
+      type: "list",
+      message: "¿Quieres que salgan errores?",
+      name: "tipo_yes_no",
+      when: (respuesta) => respuesta.tipo === "Metro",
+      choices: [
+        {
+          name: "Si",
+          value: "Si",
+        },
+        {
+          name: "No",
+          value: "No",
+        },
+      ],
+    },
+    {
+      type: "number",
+      message: "¿Que línea quieres consultar?",
+      name: "tipo_linea",
+      when: (respuesta) => respuesta.tipo === "Metro",
+    },
+    {
+      type: "list",
+      message: "¿Quieres que se te envien los datos por correo?",
+      name: "correo_si_no",
+      when: (respuesta) => respuesta.tipo === "Metro",
+      choices: [
+        {
+          name: "Si",
+          value: "Si",
+        },
+        {
+          name: "No",
+          value: "No",
+        },
+      ],
+    },
+    {
+      type: "input",
+      message: "¿A que direccion de correo lo quieres enviar?",
+      name: "correo a enviar",
+      when: (correo) => correo.correo_si_no === "Si" && correo.tipo === "Metro",
+    },
+  ]);
+  return respuestas;
+};
+
+const iniciar = async () => {
+  const respuestas = await preguntarUsuario();
+  switch (respuestas.tipo) {
+    case "Bus":
+      console.log("No hay buses disponibles");
+  }
+};
+iniciar();
